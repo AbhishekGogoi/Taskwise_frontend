b161908ba3ed7b8b3cd0a8e85e7306a3d07049b6
Navina K <navinak321@gmail.com>
2024-05-20 20:39:28 +0530

    font style changed to manrope

--- taskwise/src/App.js
+++ taskwise/src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import LandingPage from './pages/landingPage/LandingPage';
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Manrope, sans-serif',
+  },
+});
 
 function App() {
   return (
-   <LandingPage/>
+    <ThemeProvider theme={theme}>
+      <LandingPage/>
+    </ThemeProvider>
   );
 }
 
