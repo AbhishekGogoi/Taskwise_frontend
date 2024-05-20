8fbcf9ed12a2f8c97aa74b737789eadb98d399cc
Abhishek Gogoi <abhisat495@gmail.com>
2024-05-20 22:11:08 +0530

    Appbar added

--- taskwise/src/App.js
+++ taskwise/src/App.js
@@ -1,17 +1,18 @@
-
-import './App.css';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import LandingPage from './pages/landingPage/LandingPage';
+import "./App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import LandingPage from "./pages/landingPage/LandingPage";
+import Header from "./components/Header";
 const theme = createTheme({
   typography: {
-    fontFamily: 'Manrope, sans-serif',
+    fontFamily: "Manrope, sans-serif",
   },
 });
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <LandingPage/>
+      {/* <LandingPage/> */}
+      <Header />
     </ThemeProvider>
   );
 }
