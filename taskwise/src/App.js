b0ccaef561d579c446944aebcaa76c8bb2d03dc0
Pranita Turrey <64246126+pranitaT@users.noreply.github.com>
2024-05-20 20:54:49 +0530

    Updated Header with material UI changes

--- taskwise/src/App.js
+++ taskwise/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 import Header from './pages/workspace/Header';
-import Sidebar from './pages/workspace/Sidebar';
-import "@fontsource/manrope"; // Defaults to weight 400
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Sidebar />
     </div>
   );
 }
--- taskwise/src/pages/workspace/Header.js
+++ taskwise/src/pages/workspace/Header.js
@@ -1,29 +1,65 @@
+// Header.js
 import React from 'react';
-import './Header.css';
-import Logo_Image from "../../Assets/TaskWiseLogo.png";
-import Logo from "../../Assets/sample-pi.png";
-import NotificationsSharpIcon from '@mui/icons-material/NotificationsSharp';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { styled } from '@mui/system';
+import Box from '@mui/material/Box';
+import LogoImage from "../../Assets/TaskWiseLogo.png";
+import ProfileImage from "../../Assets/sample-pi.png";
 
-function Header({ accountImage }) {
+const Root = styled('div')(({ theme }) => ({
+  flexGrow: 1,
+}));
+
+const Logo = styled('img')(({ theme }) => ({
+  marginRight: theme.spacing(2),
+  width: '280px',
+  height: '100px',
+  [theme.breakpoints.down('sm')]: {
+    width: '140px',
+    height: '50px',
+  },
+}));
+
+const SectionRight = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  marginLeft: 'auto',
+}));
+
+const IconButtonStyled = styled(IconButton)(({ theme }) => ({
+  marginLeft: theme.spacing(2),
+}));
+
+const ProfileImg = styled('img')(({ theme }) => ({
+  borderRadius: '50%',
+  width: '40px',
+  height: '40px',
+  marginLeft: theme.spacing(1),
+}));
+
+const Header = () => {
   return (
-    <header className="header">
-      <div className="left-side">
-        <img src={Logo_Image} alt="Logo Icon" className="logo-icon" />
-      </div>
-      <div className="right-side">
-        <NotificationsSharpIcon className="notification-icon" />
-        <div className="account-container">
-          {accountImage ? (
-            <img src={accountImage} alt="Account" className="account-image" />
-          ) : (
-            <img src={Logo} alt="Account" className="account-image" />
-          )}
-          <ArrowDropDownIcon className="dropdown-arrow" />
-        </div>
-      </div>
-    </header>
+    <Root>
+      <AppBar position="static" sx={{ backgroundColor: 'white', color: 'black' }}>
+        <Toolbar>
+          <Logo src={LogoImage} alt="Logo" />
+          <SectionRight>
+            <IconButtonStyled color="inherit">
+              <NotificationsIcon />
+            </IconButtonStyled>
+            <IconButtonStyled color="inherit">
+              <ProfileImg src={ProfileImage} alt="Profile" />
+              <ArrowDropDownIcon />
+            </IconButtonStyled>
+          </SectionRight>
+        </Toolbar>
+      </AppBar>
+    </Root>
   );
-}
+};
 
 export default Header;
