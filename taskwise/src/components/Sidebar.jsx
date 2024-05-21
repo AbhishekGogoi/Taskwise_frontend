f437ccca92651c416337cb0025bc4e7c84d88976
Navina K <navinak321@gmail.com>
2024-05-21 11:41:33 +0530

    sidebar added

--- taskwise/src/components/Sidebar.jsx
+++ taskwise/src/components/Sidebar.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
-import { Box } from '@mui/material'
-function Sidebar() {
-  return (
-    <Box sx={{ width: 200, height: '100vh', background:'linear-gradient(180deg, #07357A 0%, #3984F3 100%)',color: 'white' }}>
+import React from 'react';
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import Projects_white from "../assets/Projects_white.png";
+import "./Sidebar.css"
 
-    </Box>
-  )
+function Sidebar() {
+    return (
+        <Box className="sidebar">
+            <List>
+                <ListItem>
+                    <ListItemButton className="list-item-button">
+                        <ListItemIcon>
+                            <img src={Projects_white} alt="Projects" className="list-item-img" />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="Projects"
+                            primaryTypographyProps={{ className: 'list-item-text' }}
+                        />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem>
+                    <ListItemButton className="list-item-button">
+                        <ListItemIcon>
+                            <img src={Projects_white} alt="Projects" className="list-item-img" />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="Workspaces"
+                            primaryTypographyProps={{ className: 'list-item-text' }}
+                        />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem>
+                    <ListItemButton className="list-item-button">
+                        <ListItemIcon>
+                            <img src={Projects_white} alt="Projects" className="list-item-img" />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="My Tasks"
+                            primaryTypographyProps={{ className: 'list-item-text' }}
+                        />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem>
+                    <ListItemButton className="list-item-button">
+                        <ListItemIcon>
+                            <img src={Projects_white} alt="Projects" className="list-item-img" />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="Calendar"
+                            primaryTypographyProps={{ className: 'list-item-text' }}
+                        />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    );
 }
 
-export default Sidebar
+export default Sidebar;
