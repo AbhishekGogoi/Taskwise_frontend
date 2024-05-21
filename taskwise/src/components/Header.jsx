6c7c32754a5e5cf0af2a38b52002aafe5c376a8b
Abhishek Gogoi <abhisat495@gmail.com>
2024-05-21 12:54:00 +0530

    header design completed

--- taskwise/src/components/Header.jsx
+++ taskwise/src/components/Header.jsx
@@ -5,7 +5,6 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
-import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
@@ -36,78 +35,65 @@ function Header() {
           <Box
             component="img"
             sx={{
-              display: { xs: "none", md: "flex" },
+              display: { md: "flex" },
               mr: 1,
-              height: { xs: 40, md: 50, lg: 60, xl: 70 },
+              height: { xs: 30, md: 40, lg: 50, xl: 60 },
             }}
             alt="Logo"
             src={LogoImage}
           />
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontSize: {
-                xs: "1.25rem",
-                md: "1.5rem",
-                lg: "1.75rem",
-                xl: "2rem",
-              },
-            }}
-          >
-            TaskWise
-          </Typography>
         </Box>
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <Box sx={{ flexGrow: 0, display: "flex", alignItems: "center" }}>
-            <Tooltip title="Notifications">
-              <IconButton sx={{ p: 1, mr: { xs: 1, md: 3, lg: 4, xl: 5 } }}>
-                <NotificationsIcon
-                  sx={{
-                    fontSize: { xs: 25, md: 30, lg: 35, xl: 40 },
-                    color: "black",
-                  }}
-                />
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ pr: 5 }}>
-                <Avatar
-                  alt="Profile Image"
-                  src={ProfileImage}
-                  sx={{
-                    width: { xs: 30, md: 40, lg: 50, xl: 60 },
-                    height: { xs: 30, md: 40, lg: 50, xl: 60 },
-                  }}
-                />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            pr: { xs: 1, md: 3, lg: 4, xl: 5 },
+          }}
+        >
+          <Tooltip title="Notifications">
+            <IconButton sx={{ p: 1, mr: { xs: 1, md: 3, lg: 4, xl: 5 } }}>
+              <NotificationsIcon
+                sx={{
+                  fontSize: { xs: 15, md: 20, lg: 25, xl: 30 },
+                  color: "black",
+                }}
+              />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 1 }}>
+              <Avatar
+                alt="Profile Image"
+                src={ProfileImage}
+                sx={{
+                  width: { xs: 15, md: 20, lg: 25, xl: 30 },
+                  height: { xs: 15, md: 20, lg: 25, xl: 30 },
+                }}
+              />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
         </Box>
       </Toolbar>
     </AppBar>
