826f45a6c13df4040f55d63fe7402d8fa0fe4c35
Abhishek Gogoi <abhisat495@gmail.com>
2024-05-25 12:42:22 +0530

    updated mytaskspage

--- taskwise/src/pages/MyTaskPage/MyTaskPage.jsx
+++ taskwise/src/pages/MyTaskPage/MyTaskPage.jsx
@@ -1,9 +1,204 @@
-import React from 'react'
+import React, { useState, useMemo } from "react";
+import { AgGridReact } from "@ag-grid-community/react"; // React Grid Logic
+import "@ag-grid-community/styles/ag-grid.css"; // Core CSS
+import "@ag-grid-community/styles/ag-theme-quartz.css"; // Theme
+import { ModuleRegistry } from "@ag-grid-community/core";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
+import { styled } from "@mui/material/styles";
 
-function MyTaskPage() {
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+const gridDiv = document.querySelector("#myGrid");
+
+const StyledAgGridContainer = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  minHeight: "100vh",
+  paddingLeft: "50px",
+  paddingTop: "40px", // Ensure container takes at least the viewport height
+});
+
+const MyTaskPage = () => {
+  const [rowData, setRowData] = useState([
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+    {
+      Task: "Design feedback on wireframe",
+      DueDate: "7 Oct",
+      Priority: "Hard",
+      Status: "Ongoing",
+      Workspace: "Book App",
+    },
+  ]);
+
+  const [colDefs, setColDefs] = useState([
+    { field: "Task" },
+    { field: "DueDate" },
+    { field: "Priority" },
+    { field: "Status" },
+    { field: "Workspace" },
+  ]);
+
+  const defaultColDef = useMemo(() => {
+    return {
+      filter: "agTextColumnFilter",
+      floatingFilter: true,
+      flex: 2,
+    };
+  }, []);
+
   return (
-    <div>MyTaskPage</div>
-  )
-}
+    <StyledAgGridContainer>
+      <div className={"ag-theme-quartz"} style={{ width: 1600, height: 600 }}>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={colDefs}
+          defaultColDef={defaultColDef}
+          rowSelection="multiple"
+          suppressRowClickSelection={true}
+        />
+      </div>
+    </StyledAgGridContainer>
+  );
+};
 
-export default MyTaskPage
+export default MyTaskPage;
