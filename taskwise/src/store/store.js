350243d890bea2155073be925558373685b94f2f
Navina K <navinak321@gmail.com>
2024-05-29 11:50:26 +0530

    slice for workspaces added

--- taskwise/src/store/store.js
+++ taskwise/src/store/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import projectreducer from "../features/project/projectSlice";
-import userreducer from "../features/user/userSlice"
+import userreducer from "../features/user/userSlice";
+import workspacereducer from "../features/workspace/workspaceSlice";
 
 const store=configureStore({
     reducer: {
         project:projectreducer,
-        user:userreducer
+        user:userreducer,
+        workspace:workspacereducer
     }
 })
 export default store
