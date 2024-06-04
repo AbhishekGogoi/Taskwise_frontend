e51cd6cd2ba04c4a81a68190c28cadbc5c52fb3a
Abhishek Gogoi <abhisat495@gmail.com>
2024-06-04 21:43:13 +0530

    sidebar change reverted

--- taskwise/src/components/Sidebar.jsx
+++ taskwise/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   Box,
   List,
@@ -11,45 +11,10 @@ import Projects_white from "../assets/Projects_white.png";
 import WorkspaceIconWhite from "../assets/WorkspaceIconWhite.png";
 import MyTaskIconWhite from "../assets/MyTaskIconWhite.png";
 import CalendarWhite from "../assets/CalendarWhite.png";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Sidebar.css";
 
 function Sidebar() {
-  const location = useLocation();
-  const [selectedIndex, setSelectedIndex] = useState(() => {
-    switch (location.pathname) {
-      case "/projects":
-        return 0;
-      case "/workspaces":
-        return 1;
-      case "/my-tasks":
-        return 2;
-      case "/calendar":
-        return 3;
-      default:
-        return 0;
-    }
-  });
-
-  useEffect(() => {
-    switch (location.pathname) {
-      case "/projects":
-        setSelectedIndex(0);
-        break;
-      case "/workspaces":
-        setSelectedIndex(1);
-        break;
-      case "/my-tasks":
-        setSelectedIndex(2);
-        break;
-      case "/calendar":
-        setSelectedIndex(3);
-        break;
-      default:
-        setSelectedIndex(0);
-    }
-  }, [location.pathname]);
-
   return (
     <Box className="sidebar">
       <List>
@@ -57,10 +22,7 @@ function Sidebar() {
           <ListItemButton
             component={Link}
             to="/projects"
-            className={`list-item-button ${
-              selectedIndex === 0 ? "selected" : ""
-            }`}
-            onClick={() => setSelectedIndex(0)}
+            className="list-item-button"
           >
             <ListItemIcon>
               <img
@@ -79,10 +41,7 @@ function Sidebar() {
           <ListItemButton
             component={Link}
             to="/workspaces"
-            className={`list-item-button ${
-              selectedIndex === 1 ? "selected" : ""
-            }`}
-            onClick={() => setSelectedIndex(1)}
+            className="list-item-button"
           >
             <ListItemIcon>
               <img
@@ -101,10 +60,7 @@ function Sidebar() {
           <ListItemButton
             component={Link}
             to="/my-tasks"
-            className={`list-item-button ${
-              selectedIndex === 2 ? "selected" : ""
-            }`}
-            onClick={() => setSelectedIndex(2)}
+            className="list-item-button"
           >
             <ListItemIcon>
               <img
@@ -123,10 +79,7 @@ function Sidebar() {
           <ListItemButton
             component={Link}
             to="/calendar"
-            className={`list-item-button ${
-              selectedIndex === 3 ? "selected" : ""
-            }`}
-            onClick={() => setSelectedIndex(3)}
+            className="list-item-button"
           >
             <ListItemIcon>
               <img
