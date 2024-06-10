98f8211d8324acff0a825506f25faf69fcfcafb1
Navina K <99734360+navina1@users.noreply.github.com>
2024-06-10 08:02:24 +0530

    Aibranch (#32)
    * assigneeuserId, and comment feild added in task creation
    
    * removed top portion
    
    * AI page design changes
    
    * removed text feilds
    
    * changes added
    
    * prompt added
    
    * changes in prompt added
    
    * validation added for AI page

--- taskwise/src/pages/AI/AIInputPage.jsx
+++ taskwise/src/pages/AI/AIInputPage.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { Box, TextField, Button, Typography, Paper } from "@mui/material";
+import { Box, TextField, Button, Typography, FormControl, InputLabel, Select, MenuItem,FormHelperText } from "@mui/material";
 import { styled } from "@mui/system";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { createProjectAIASync } from "../../features/AI/projectAISlice";
-
+import IllustrationImage from "../../assets/IllustrationImage.jpeg"
 const Container = styled(Box)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
@@ -21,7 +21,6 @@ const WhiteBox = styled(Box)(({ theme }) => ({
   alignItems: "flex-start",
   justifyContent: "space-between",
   width: "98%",
-  height: "65vh",
   padding: theme.spacing(2),
   backgroundColor: "#fff",
   borderRadius: theme.shape.borderRadius,
@@ -30,101 +29,183 @@ const WhiteBox = styled(Box)(({ theme }) => ({
   paddingTop: "4rem",
 }));
 
-const FormBox = styled(Box)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  gap: theme.spacing(8),
-  width: "30%",
-  paddingLeft: "3rem",
-}));
-
-const ImageBox = styled(Box)(({ theme }) => ({
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  width: "50%",
-}));
 
 const AIInputPage = () => {
   const dispatch = useDispatch();
+  const workspaces = useSelector((state) => state?.workspace?.workspaces);
+  const [workspace, setWorkspace] = useState('');
+  const [projectName, setProjectName] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [errors, setErrors] = useState({});
 
-  const [prompt, setPrompt] = useState("");
+  const validate = () => {
+    let tempErrors = {};
+    tempErrors.projectName = projectName ? "" : "Project Name is required";
+    tempErrors.description = description ? "" : "Project Description is required";
+    tempErrors.workspace = workspace ? "" : "Workspace assignment is required";
+    setErrors(tempErrors);
+    return Object.values(tempErrors).every(x => x === "");
+  };
 
-  const handleButtonClick = () => {
-    const data = {
-      prompt: prompt,
-    };
-    dispatch(createProjectAIASync(data));
+  const handleButtonClick = async () => {
+    if (validate()) {
+      const data = {
+        prompt: `I am building a "${projectName}" project. Come up with
+        set of development tasks  needed to build this project.
+         give the result in json format example as as below.{
+         project : ${projectName},
+         description : ${description},
+         tasks: [
+           {
+             title : "Setup Repo",
+             description : "setup both fronttend and backed repo "
+           },
+           {
+             title : "Config Database",
+             description : "setup Database Mysql "
+           }
+         ] create minimum 15 task`,
+      };
+      const res = await dispatch(createProjectAIASync(data));
+      console.log(res)
+    }
+
+  };
+  const handleWorkspaceChange = (event) => {
+    const { value } = event.target;
+    setWorkspace(value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      workspace: value ? "" : "Workspace assignment is required",
+    }));
+  };
+  const handleProjectNameChange = (event) => {
+    const { value } = event.target;
+    setProjectName(value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      projectName: value ? "" : "Project Name is required",
+    }));
+  };
+
+  const handleDescriptionChange = (event) => {
+    const { value } = event.target;
+    setDescription(value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      description: value ? "" : "Project Description is required",
+    }));
   };
 
   return (
     <>
-      <Paper
-        elevation={3}
-        sx={{
-          width: "100%",
-          height: 80,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography
-          variant="body1"
-          component="div"
-          sx={{ p: 2, fontWeight: "bold" }}
-        >
-          Workspace A
-        </Typography>
-      </Paper>
-      <Container>
-        <WhiteBox>
-          <FormBox>
-            <Typography
-              variant="h6"
-              component="h3"
-              gutterBottom
-              sx={{ paddingLeft: "6rem", color: "#3780ED" }}
-            >
-              Create Project with TaskWise AI
-            </Typography>
-            <TextField
-              label="Enter Project Name"
-              variant="outlined"
-              fullWidth
-            />
-            <TextField
-              label="Enter Project Description"
-              variant="outlined"
-              multiline
-              rows={4}
-              fullWidth
-              value={prompt}
-              onChange={(e) => setPrompt(e.target.value)}
-            />
-            <TextField label="Enter Team Size" variant="outlined" fullWidth />
-            <Button
-              variant="contained"
+      <Container >
+        <WhiteBox >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: { xs: "column", md: "row" },
+              alignItems: "center",
+            }}
+          >
+            <Box sx={{
+              flex: 1,
+              padding: '2rem',
+              order: { xs: 2, md: 1 },
+            }}>
+              <Typography
+                variant="h6"
+                component="h3"
+                gutterBottom
+                sx={{
+                  paddingLeft: "6rem",
+                  color: "#3780ED",
+                  mb: 4
+                }}
+              >
+                Create Project with TaskWise AI
+              </Typography>
+              <TextField
+                label="Enter Project Name"
+                value={projectName}
+                variant="outlined"
+                onChange={handleProjectNameChange}
+                fullWidth
+                error={!!errors.projectName}
+                helperText={errors.projectName}
+                sx={{ marginBottom: "1.6rem" }}
+              />
+              <TextField
+                label="Enter Project Description"
+                variant="outlined"
+                multiline
+                rows={4}
+                fullWidth
+                value={description}
+                error={!!errors.description}
+                helperText={errors.description}
+                onChange={handleDescriptionChange}
+                sx={{ marginBottom: "1.6rem" }}
+              />
+              <FormControl
+                fullWidth
+                margin="normal"
+                style={{ marginBottom: "40px" }}
+                error={!!errors.workspace}
+              >
+                <InputLabel id="assign-workspace-label">
+                  Assign Workspace
+                </InputLabel>
+                <Select
+                  labelId="assign-workspace-label"
+                  id="assign-workspace"
+                  label="Assign Workspace"
+                  value={workspace}
+                  onChange={handleWorkspaceChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  {workspaces.map((workspace) => (
+                    <MenuItem key={workspace.id} value={workspace.id}>
+                      {workspace.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+                {!!errors.workspace && <FormHelperText>{errors.workspace}</FormHelperText>}
+              </FormControl>
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "#00c6ff",
+                  backgroundImage: "linear-gradient(120deg, #00c6ff, #8e71df)",
+                  color: "#fff",
+                  fontWeight: "bold",
+                  borderRadius: "10px",
+                  width: "100%",
+                }}
+                onClick={handleButtonClick}
+              >
+                Generate with TaskWise AI
+              </Button>
+            </Box>
+            <Box
               sx={{
-                backgroundColor: "#00c6ff",
-                backgroundImage: "linear-gradient(120deg, #00c6ff, #8e71df)",
-                color: "#fff",
-                fontWeight: "bold",
-                borderRadius: "10px",
+                flex: 1,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: '2rem',
+                order: { xs: 1, md: 2 },
               }}
-              fullWidth
-              onClick={handleButtonClick}
             >
-              Generate with TaskWise AI
-            </Button>
-          </FormBox>
-          <ImageBox>
-            <img
-              src="illustration-url" // Replace with your illustration image URL or import
-              alt="Illustration"
-              style={{ maxWidth: "100%", height: "auto" }}
-            />
-          </ImageBox>
+              <img
+                src={IllustrationImage} // Replace with your illustration image URL or import
+                alt="Illustration"
+                style={{ maxWidth: "80%", height: "auto", backgroundColor: "transparent" }}
+              />
+            </Box>
+          </Box>
         </WhiteBox>
       </Container>
     </>
