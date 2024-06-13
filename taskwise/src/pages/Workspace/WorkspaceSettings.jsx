352ddea49a94278e882f207669b5e053a97de534
Pranita Turrey <64246126+pranitaT@users.noreply.github.com>
2024-06-13 09:51:28 +0530

    correction

--- taskwise/src/pages/Workspace/WorkspaceSettings.jsx
+++ taskwise/src/pages/Workspace/WorkspaceSettings.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import {
   Box, Grid, IconButton, Typography, Paper, TextField, Dialog, DialogContent, DialogTitle, Tabs, Tab, Card, CardContent, CardActions, Modal,
   CardMedia
